ebcee1055f0e6d179dc9181e9ba5d7d564421953
Anant Katyayn <121535961+anantkatyayn@users.noreply.github.com>
2024-08-15 17:31:51 +0000

    strings JS tested

---
+++ JSBasics/Strings.js
@@ -0,0 +1,9 @@
+const newName = new String("AnantKatyayn");
+const newURL = new String("https://anantkatyayn.com/local/anant")
+
+console.log(newName.charAt(2));
+console.log(newName.indexOf("t"));
+
+console.log(newName[2]);
+
+console.log(newURL.split("/")[3]);
