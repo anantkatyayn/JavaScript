cb58a5b2ec7977625381829c6abad60e6c325bbf
Anant Katyayn <121535961+anantkatyayn@users.noreply.github.com>
2024-08-16 13:37:38 +0000

    Practiced Date and Time

---
+++ JSBasics/DateAndTime.js
@@ -0,0 +1,25 @@
+
+
+/* function to find elapsed time of a function.*/
+
+// function getElapsedTime(func){
+//     const startTime = new Date();
+//     const d = func();
+//     const endTime = new Date();
+
+//     console.log(`Time elapsed is ${String(endTime - startTime)} milliseconds`);
+// }
+
+// function hola(){
+//     for (var i = 0; i<= 10000; i++){
+//         console.log("holaAmigo");
+        
+//     }
+// }
+
+// console.log(getElapsedTime(hola));
+
+
+
+
+
--- JSBasics/NumberAndMaths.js
+++ JSBasics/NumberAndMaths.js
@@ -1,8 +1,16 @@
 
 
-const num = new Number(540000.655);
-console.log(num);
+// const num = new Number(540000.655);
+// console.log(num);
 
-console.log(num.toLocaleString("en-IN"));
-console.log(num.toLocaleString("de-DE"));
+// console.log(num.toLocaleString("en-IN"));
+// console.log(num.toLocaleString("de-DE"));
 
+
+
+const min = 0;
+const max = 100;
+
+// console.log((Math.floor((Math.random)*(max - min + 1))) + min)
+
+console.log(Math.floor((Math.random() * (max - min + 1)) + min));
