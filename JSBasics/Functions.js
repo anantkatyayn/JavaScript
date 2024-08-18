6a36d6c580f633d5abe78df48627b0af5629599a
Anant Katyayn <121535961+anantkatyayn@users.noreply.github.com>
2024-08-18 18:48:04 +0000

    Completed Scope Basics

--- JSBasics/Functions.js
+++ JSBasics/Functions.js
@@ -1,8 +1,8 @@
 // adding two numbers only if arguements passed are numbers
 
-function addTwoNums(num1 , num2){
-    if(typeof(num1) === 'number'){
-        if (typeof(num2) === 'number') {
+function addTwoNums(num1 = 7 , num2){                       //to define a default value of parameter 
+    if(typeof(num1) === "number"){
+        if (typeof(num2) === "number") {
             return num1+num2;
         } else{
             console.log("Invalid Input1");
@@ -14,7 +14,19 @@ function addTwoNums(num1 , num2){
     }
 }
 
-console.log(addTwoNums( 5, 7 ))
+// console.log(addTwoNums( 5, 6))
 
 // var a = 7
-// console.log(typeof(a));
+// console.log(typeof(a)); 
+
+
+
+// REST OPERATOR = "...", Rest and Spread operator are same syntactically but according to usecase their functionality change.
+//rest operator take as many number of inputs and bundles them in an array while spread operator makes elements of array as single entity.
+
+
+function useRest(...num){
+    return num
+}
+
+console.log(useRest(400,500,7000,400,520))
---
+++ JSBasics/Scope.js
@@ -0,0 +1,10 @@
+let a = 520
+function hola(){
+    let a = 20;
+    let b = 30;
+    let c = 400;
+    console.log(`Inner console.log ${a}`)
+}
+// hola()
+// console.log(`outer ${a}`)
+
