d832be1abd8422c7d445a03a963626c81fe2344a
Anant Katyayn <121535961+anantkatyayn@users.noreply.github.com>
2024-08-30 12:44:26 +0000

    Practiced till for-of loop

---
+++ JSBasics/loops.js
@@ -0,0 +1,34 @@
+// For loop syntax:
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+// break statement: terminates the loop once break statement is encountered.
+// continue statement: skips that particular condition if continue occurs.
+
+
+
+//While loop syntax:
+// while (condition) {
+    // code block
+// }
+
+// do-while loop syntax:
+// do {
+    // code block
+// } while (condition);
+
+
+const arr = ["Batman", "Superman", "Flash", "GreenLantern"]
+//for-of loop syntax:
+for (const element of arr) {
+    // console.log(element);
+    
+}
+
+let str = "Hola Amigo"
+// console.log(str.split(""))
+for (const element of str) {
+    
+}
