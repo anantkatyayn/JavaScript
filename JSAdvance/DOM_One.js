2dbe0b2e004d9c0deeb8a6057b9aa529ba2fd2fe
Anant Katyayn <121535961+anantkatyayn@users.noreply.github.com>
2024-09-17 04:29:02 +0000

    Practiced query selectors

---
+++ JSAdvance/DOM_One.js
@@ -0,0 +1,19 @@
+// these returns a nodelist of selected elements.
+
+// document.querySelector('.className')
+// document.querySelector('#ID')
+// document.querySelector('element')
+
+// document.querySelectorAll('.className')
+// document.querySelectorAll('#ID')
+// document.querySelectorAll('element')
+
+// *** you can use forEach on NodeList directly or convert into array for other loops*** //
+// -------------------------------------------------------------------------------------------------------- //
+// these return HTML collection of selected items
+
+// document.getElementById('id')
+// document.getElementsByClassName('className')
+
+// ***Convert html collections to arrays usinf "Array.from()" in order to apply loops on data in HTML collection.*** //
+// -------------------------------------------------------------------------------------------------------- //
