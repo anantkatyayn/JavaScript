2bbb90e0989de389ab676a41b1a984a2f8cba1ac
Anant Katyayn <121535961+anantkatyayn@users.noreply.github.com>
2025-03-08 07:18:51 +0000

    practiced switch case statements and nullish coalescing operator

---
+++ JSBasics/ControlFlow/SwitchCase.js
@@ -0,0 +1,50 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// let month = "jan";
+
+// switch(month){
+//     case "jan":
+//         console.log(`this is January`)
+//         break;
+//     case "feb":
+//         console.log(`this is feb`)
+//         break;
+//     case "march":
+//         console.log(`this is march`)
+//         break;
+//     case "april":
+//         console.log(`this is april`)
+//         break;
+//     case "may":
+//         console.log(`this is may`)
+//         break;
+//     case "june":
+//         console.log(`this is june`)
+//         break;
+//     case "july":
+//         console.log(`this is july`)
+//         break;
+
+//     default:
+//         console.log(`Sorry man, this isn't your calendar, try a different planet`)
+//         break;
+// }
+
+
+
+//  nullish coalescing operator "??"
+
+let val1;
+// val1 = null??10;                //output: 10
+// val1 = 15??10;               //output: 15
+// val1 = undefined??10;        //output: 10
+// val1 = null??20??10;         //output: 20
+
+// console.log(val1)
