a533b38c2baf1423ddde5ea29fac8a4b5bdb0170
Anant Katyayn <121535961+anantkatyayn@users.noreply.github.com>
2025-03-15 04:01:06 +0000

    practiced forEach loop on array of objects

---
+++ JSBasics/loops03.js
@@ -0,0 +1,18 @@
+const myLanguages = [
+    {
+        name:"JavaScript",
+        extention : "js",
+    },
+    {
+        name:"Python",
+        extention : "py",
+    },
+    {
+        name:"C++",
+        extention : "cpp",
+    },
+]
+
+myLanguages.forEach((item)=>{
+    console.log(item.name)
+})
